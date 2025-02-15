340a6132795ebc6edd1bd4671fd7a40ffb806945
cy-cheng <cycheng.dev@gmail.com>
2025-02-15 22:26:44 +0800

    add some examination features (WIP)

--- home.py
+++ home.py
@@ -6,6 +6,7 @@ st.set_page_config(layout="wide")
 pages = [
     st.Page("page/config.py", title="病患設定", icon="🔧"),
     st.Page("page/test.py", title="看診區", icon="🩺"),
+    st.Page("page/examination.py", title="檢查區", icon="🧪"),
     st.Page("page/grade.py", title="評分區", icon="📝"),
 ]
 
